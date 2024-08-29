6cbf4a729af8d2b5faafeee4e4865fc1b5cdfdb3
zahra <zahrarahimi.abc@gmail.com>
2024-08-29 19:38:15 +0330

    class Library added

--- library_management_system.py
+++ library_management_system.py
@@ -6,12 +6,12 @@ class Book:
         self.available = available
 
     def __str__(self):
-        return f'title: {self.title}, author: {self.author}, isbn: {self.isbn}, avalable: {self.available}'
+        return f'title: {self.title}, author: {self.author}, isbn: {self.isbn}, available: {self.available}'
 
     def borrow(self):
         self.available = False
 
-    def return_b(self):
+    def return_book(self):
         self.available = True
 
 
@@ -30,4 +30,38 @@ class Member:
 
     def return_book(self, book):
         self.borrowed_books.remove(book)
-        book.return_b()
+        book.return_book()
+
+
+class Library:
+    def __init__(self, books = [], members = []):
+        self.books = books
+        self.members = members
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def register_member(self, member):
+        self.members.append(member)
+
+    def issue_book(self, member_id, isbn):
+        for book in self.books:
+            if book.isbn == isbn:
+                inp_book = book
+                break
+        for member in self.members:
+            if member.member_id == member_id:
+                inp_member = member
+                break
+        inp_member.borrow_book(inp_book)
+    
+    def return_book(self, member_id, isbn):
+        for book in self.books:
+            if book.isbn == isbn:
+                inp_book = book
+                break
+        for member in self.members:
+            if member.member_id == member_id:
+                inp_member = member
+                break
+        inp_member.return_book(inp_book)
