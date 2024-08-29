559213c55fd7d0d24e817ff916eadcc9152955f3
zahra <zahrarahimi.abc@gmail.com>
2024-08-29 18:23:25 +0330

    class Member added

--- library_management_system.py
+++ library_management_system.py
@@ -11,5 +11,23 @@ class Book:
     def borrow(self):
         self.available = False
 
-    def return_book(self):
+    def return_b(self):
         self.available = True
+
+
+class Member:
+    def __init__(self, name, member_id, borrowed_books = []):
+        self.name = name
+        self.member_id = member_id
+        self.borrowed_books = borrowed_books
+
+    def __str__(self):
+        return f'name: {self.name}, member id: {self.member_id}, borrowed books: {self.borrowed_books}'
+
+    def borrow_book(self, book):
+        self.borrowed_books.append(book)
+        book.borrow()
+
+    def return_book(self, book):
+        self.borrowed_books.remove(book)
+        book.return_b()
